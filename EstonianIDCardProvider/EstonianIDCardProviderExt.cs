6d92ba438f833ea149c46115df61ea4c82d51da8
Stig Voss <stig.voss@gmail.com>
2016-12-29 21:29:43 +0200

    Base structure prepared Main class created Provider class created

---
+++ EstonianIDCardProvider/EstonianIDCardProviderExt.cs
@@ -0,0 +1,28 @@
+﻿using KeePass.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstonianIDCardProvider
+{
+    public sealed class EstonianIDCardProviderExt : Plugin
+    {
+        private IPluginHost _host = null;
+        private SmartCardProvider _provider = new SmartCardProvider();
+
+        public override bool Initialize(IPluginHost host)
+        {
+            _host = host;
+            _host.KeyProviderPool.Add(_provider);
+
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            _host.KeyProviderPool.Remove(_provider);
+        }
+    }
+}
---
+++ EstonianIDCardProvider/SmartCardProvider.cs
@@ -0,0 +1,27 @@
+﻿using KeePassLib.Keys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstonianIDCardProvider
+{
+    public sealed class SmartCardProvider : KeyProvider
+    {
+        private const string PROVIDER_NAME = "Estonian ID Card Provider";
+
+        public override string Name
+        {
+            get
+            {
+                return PROVIDER_NAME;
+            }
+        }
+
+        public override byte[] GetKey(KeyProviderQueryContext ctx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
